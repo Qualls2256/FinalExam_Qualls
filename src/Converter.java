c73084f74e40c2b20521df47c412dd7c086f208b
orthu <orthu@DESKTOP-JIJE882>
2017-05-19 13:56:09 -0600

    is here now

--- src/Converter.java
+++ src/Converter.java
@@ -1,18 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class Converter {
 
 	public String changeToHex(int intBase10) {
 		String answer = "";
 		// code for you to complete
 		if (intBase10 < 0 || intBase10 > 255) {
-			// JOP "Error, range outside of 0-255, please try again
+			JOptionPane.showMessageDialog(null, "Error, range outside of 0-255, please try again");
 			return null;
 		}
+		answer = Integer.toHexString(intBase10).toUpperCase();
 
-		// basic test
-		if (intBase10 == 0)
-			answer = "00";
-		if (intBase10 == 255)
-			answer = "FF";
 		return answer;
 	}
 
